e7cf5daec6b36e349f0cf98ac6c2da695578db42
Billy Truong <billytruong2001@yahoo.com>
2012-06-09 19:15:53 -0700

    Test commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+	before_filter :set_locale
+
+	protect_from_forgery
+  include SessionsHelper
+
+	def set_locale
+	  I18n.locale = params[:locale] || I18n.default_locale
+	end
+
+  def default_url_options(options={})
+	  logger.debug "default_url_options is passed options: #{options.inspect}\n"
+	  { :locale => I18n.locale }
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRTutCh3::Application.config.secret_token = 'ed25067b82a1c3f517d8a4d813c6e603550bc046ff45fe1bc8ea2b7cb346d9796bf8bf81d9892af287dfda07d29eeb374b1d7bbacf9541544a712046deaad235'
