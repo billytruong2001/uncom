5caa3af63833a1a8d773b14961f3ef7905d502b6
Billy Truong <billytruong2001@yahoo.com>
2012-06-12 16:00:04 -0700

    456

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,21 +36,8 @@ namespace :deploy do
     task command, roles: :app, except: {no_release: true} do
       run "#{sudo} /etc/init.d/apache2 #{command}"
     end
+    run "touch #{current_path}/tmp/restart.txt"
   end
-
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
-  end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
